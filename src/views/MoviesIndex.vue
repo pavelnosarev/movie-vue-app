7f0a5fbf927751375a773dd0cf0d28024f053eba
“Pavel <itspavelnosarev@gmail.com>
2021-07-30 11:48:24 -0400

    added filter

--- src/views/MoviesIndex.vue
+++ src/views/MoviesIndex.vue
@@ -1,22 +1,29 @@
 <template>
   <div class="movies-index">
-    <div v-for="movie in movies" :key="movie.id">
-      <router-link v-bind:to="`/movies/${movie.id}`">
-        <h2>Title: {{ movie.title }}</h2>
-        <p>Director: {{ movie.director }}</p>
-        <img v-bind:src="movie.image_url" alt="movie.title" />
-      </router-link>
+    Search by title:
+    <input v-model="titleFilter" />
+    <div v-for="movie in filterBy(movies, titleFilter, 'title')">
+      <div v-for="movie in movies" :key="movie.id">
+        <router-link v-bind:to="`/movies/${movie.id}`">
+          <h2>Title: {{ movie.title }}</h2>
+          <p>Director: {{ movie.director }}</p>
+          <img v-bind:src="movie.image_url" alt="movie.title" />
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 export default {
   data: function () {
     return {
       message: "Here are all the movies!",
       movies: [],
+      mixins: [Vue2Filters.mixin],
+      titleFilter: "",
     };
   },
   created: function () {
